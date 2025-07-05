35eefe4dbf1daaa630d11109922eb97d4d24d4b6
Aleks Cela <alekscela7@gmail.com>
2025-07-05 11:42:59 +0200

    init

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,24 @@
+import { Link } from "react-router-dom";
 
-
-function Cart(){
-
-    return (
-        <div>
-            Hello World 
-            Welcome to Cart
-        </div>
-
-
-
-    )
-
+function Cart({ cart }) {
+	return (
+		<div>
+			<h1>Shopping Cart</h1>
+			{cart.length === 0 ? (
+				<p>Your cart is empty.</p>
+			) : (
+				<ul>
+					{cart.map((item, index) => (
+						<li key={index}>
+							{item.title} - ${item.price} (Quantity:{" "}
+							{item.quantity})
+						</li>
+					))}
+				</ul>
+			)}
+			<Link to='/'>Back to Product Catalog</Link>
+		</div>
+	);
 }
 
 export default Cart;
--- src/components/ProductCatalog.jsx
+++ src/components/ProductCatalog.jsx
@@ -1,20 +1,25 @@
-import { Link } from "react-router-dom";    
+import { Link } from "react-router-dom";
+import ProductCard from "./ProductCard";
+import AddProductForm from "./AddProductForm";
 
-function ProductCatalog(){
-
-    return (
-        <div>
-            Hello World 
-
-            Welcome to Product Catalog
-
-            <Link to="/Cart"> Kliko per te Cart</Link>
-        </div>
-
-
-
-    )
-
+function ProductCatalog({ productCatalog, addProduct, addToCart, cartNumber }) {
+	return (
+		<div>
+			<h1>Product Catalog</h1>
+			<Link to='/Cart'> CART {cartNumber}</Link>
+			{productCatalog.map((product) => (
+				<ProductCard
+					key={product.id}
+					product={product}
+					addToCart={addToCart}
+				/>
+			))}
+			<AddProductForm
+				productCatalog={productCatalog}
+				addProduct={addProduct}
+			/>
+		</div>
+	);
 }
 
 export default ProductCatalog;
